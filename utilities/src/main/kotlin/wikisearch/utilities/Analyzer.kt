0b39862e7ba77fb30cc821fb720a5fce1b90bfd1
Semere Habtu <se.semere.habtu@gmail.com>
2023-03-13 01:05:48 +0300

    Create a wrapper for text normalization functions

--- utilities/src/main/kotlin/wikisearch/utilities/Analyzer.kt
+++ utilities/src/main/kotlin/wikisearch/utilities/Analyzer.kt
@@ -1,4 +1,13 @@
 package wikisearch.utilities
 
 class Analyzer {
+    companion object {
+        fun analyze(text: String): List<String> {
+            val tokens = Stemmer.tokenize(text)
+            val lowercase = Stemmer.lowercaseFilter(tokens)
+            val punctuationFiltered = Stemmer.punctuationFilter(lowercase)
+            val stopWordFiltered = Stemmer.stopWordFilter(punctuationFiltered)
+            return Stemmer.stemFilter(stopWordFiltered)
+        }
+    }
 }
